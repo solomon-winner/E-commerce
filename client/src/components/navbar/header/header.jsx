f4230cf4f59afde13feb60d67b21a11bdf18a5cb
solomon-winner <solyalew3@gmail.com>
2024-01-09 22:08:41 +0300

    ...

---
+++ client/src/components/navbar/header/header.jsx
@@ -0,0 +1,45 @@
+import "./header.css"
+
+const Header = () => {
+    return (
+        
+        <div className Name="header">
+            <div className ="header-top ">
+                <div className ="container">
+                    <div className ="wrapper flexitem">
+                        <div className ="left">
+                            <ul className ="flexitem main-links">
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Featured products</a></li>
+                                <li><a href="#">wishlist</a></li>
+                            </ul>
+                        </div>
+                        <div className ="right ">
+                            <ul className ="flexitem main-links">
+                                    <li><a href="#">Sign Up</a></li>
+                                    <li><a href="#">My Account</a></li>
+                                    <li><a href="#">Order Tracking</a></li>
+                                    <li><a href="#">USD <span className ="icon-small"><i className ="ri-arrow-down-s-line"></i></span></a>
+                                    <ul>
+                                        <li className ="current"><a href="#">USD</a></li>
+                                        <li><a href="#">BIRR</a></li>
+                                        <li><a href="#">GBR</a></li>
+                                        <li><a href="#">EURO</a></li>
+                                    </ul>
+                                    </li>
+                                    <li><a href="#">English <span className ="icon-small"><i className ="ri-arrow-down-s-line"></i></span></a>
+                                    <ul>
+                                        <li className ="current"><a href="#">English</a></li>
+                                        <li><a href="#">Amharic</a></li>
+                                        <li><a href="#">Oromifa</a></li>
+                                    </ul>
+                                    </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+)
+    }
+    export default Header;
