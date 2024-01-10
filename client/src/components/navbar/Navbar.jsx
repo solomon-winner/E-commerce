2babea10d2ce2dddb2bde93119137a97e8be6b84
solomon-winner <solyalew3@gmail.com>
2024-01-10 14:45:59 +0300

    ...

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css"
 import Header from "./header/header"
+import HeadList from "./headList/headList"
 const Navbar = () => {
     return (
         
 <div className="Nav">
     <Header/>
+    <HeadList/>
 </div>
 )
     }
--- client/src/components/navbar/headList/headList.jsx
+++ client/src/components/navbar/headList/headList.jsx
@@ -2,7 +2,55 @@ import "./headList.css"
 
 const HeadList = () => {
     return (
-        <div className="headList"></div>
+        <div className="headList">
+            
+        <div className ="container">
+            <div className ="wrapper flexitem">
+                <a href="" className ="trigger desktop-hide"><span className ="i ri-menu-2-line"></span></a>
+                <div className ="left flexitem">
+                    <div className ="logo"><a href="/"><span className ="circle"></span>.suk</a></div>
+                    <nav className ="mobile-hide">
+                        <ul className ="flexitem second-links">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">shope</a></li>
+                            <li className  = "has-child">
+                                <a href="#">Women
+                            <div className ="icon-small"><i className ="ri-arrow-down-s-line"></i></div>
+
+                            </a>
+
+                            </li>
+                            <li><a href="#">Men</a></li>
+                            <li>
+                                <a  href="#">Sports
+                                <div className = "fly-item"><span>New!</span></div>
+                            </a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+                <div className = "right">
+                    <ul className = "flexitem second-links">
+                        <li className   = " mobile-hide "><a href="">
+                            <div className = "icon-large"><i className = "ri-heart-line"></i></div>
+                            <div className = "fly-item"><span className = "item-number">0</span></div>
+                        </a></li>
+                        <li><a href="" className   = "iscart">
+                            <div className = "icon-large">
+                                <i className = "ri-shopping-cart-line"></i>
+                                <div className = "fly-item"><span className = "item-number">0</span></div>
+
+                            </div>
+                            <div className = "icon-text">
+                                <div className = "mini-text">Total</div>
+                                <div className = "cart-total">$0.00</div>
+                            </div>
+                        </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
     )
 }
 export default HeadList;
