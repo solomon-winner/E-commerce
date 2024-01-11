59709ea3824ea43295eef423a7b5f652a9d01758
solomon-winner <solyalew3@gmail.com>
2024-01-11 13:42:27 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Nav from "./components/navbar/Navbar.jsx";
 import Banner from "./components/Banner/banner"
+import Image1 from "./assets/img/banner1.jpg"
+import Image2 from "./assets/img/banner2.jpg"
+import Image3 from "./assets/img/banner3.jpg"
+import Image4 from "./assets/img/banner4.jpg"
 
 function App() {
+  const images = [Image1, Image2, Image3,Image4]
   return (
     <div className="App">
      <Nav/>
-     <Banner/>
+     <Banner images = {images} interval={6000}/>
     </div>
   );
 }
