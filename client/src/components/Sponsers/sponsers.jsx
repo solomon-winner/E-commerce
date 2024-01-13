de2b82a3a6f630ad455d1e5aee1ee6ab3e4d1b0e
solomon-winner <solyalew3@gmail.com>
2024-01-13 19:59:12 +0300

    ...

--- client/src/components/Sponsers/sponsers.jsx
+++ client/src/components/Sponsers/sponsers.jsx
@@ -3,15 +3,47 @@ import Nike from "../../assets/icons/Nike.jpg"
 
 const Sponsers = () => {
     return (
-        <div className="sponsers">
-            <img src= {Nike} alt="" />
-            <img src= {Nike} alt="" />
-            <img src= {Nike} alt="" />
-            <img src= {Nike} alt="" />
-            <img src= {Nike} alt="" />
-            <img src= {Nike} alt="" />
+        <div className ="sponsers">
+        <div className ="container">
+            <div className ="wrapper flexitem">
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/asus.png" alt=""/>
+                    </a>
+                </div>
 
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/zara.png" alt=""/>
+                    </a>
+                </div>
+
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/samsung.png" alt=""/>
+                    </a>
+                </div>
+
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/oppo.png" alt=""/>
+                    </a>
+                </div>
+
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/hurley.png" alt=""/>
+                    </a>
+                </div>
+
+                <div className ="item">
+                    <a href="#">
+                        <img src="./assets/img/brands/dng.png" alt=""/>
+                    </a>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 export default Sponsers;
