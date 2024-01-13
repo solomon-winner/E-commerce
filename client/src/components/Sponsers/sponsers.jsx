fd0c51344024ae73550a6d1556fb624427eec842
solomon-winner <solyalew3@gmail.com>
2024-01-13 20:05:19 +0300

    ...

--- client/src/components/Sponsers/sponsers.jsx
+++ client/src/components/Sponsers/sponsers.jsx
@@ -1,5 +1,10 @@
 import "./sponsers.css"
-import Nike from "../../assets/icons/Nike.jpg"
+import Asus from "../../assets/img/brands/asus.png"
+import Dng from "../../assets/img/brands/dng.png"
+import Hurley from "../../assets/img/brands/hurley.png"
+import Oppo from "../../assets/img/brands/oppo.png"
+import Samsung from "../../assets/img/brands/samsung.png"
+import Zara from "../../assets/img/brands/zara.png"
 
 const Sponsers = () => {
     return (
@@ -8,31 +13,31 @@ const Sponsers = () => {
             <div className ="wrapper flexitem">
                 <div className ="item">
                     <a href="#">
-                        <img src="./assets/img/brands/asus.png" alt=""/>
+                        <img src = {Asus} alt=""/>
                     </a>
                 </div>
 
                 <div className ="item">
                     <a href="#">
-                        <img src="./assets/img/brands/zara.png" alt=""/>
+                        <img src = {Dng} alt=""/>
                     </a>
                 </div>
 
                 <div className ="item">
                     <a href="#">
-                        <img src="./assets/img/brands/samsung.png" alt=""/>
+                        <img src = {Hurley} alt=""/>
                     </a>
                 </div>
 
                 <div className ="item">
                     <a href="#">
-                        <img src="./assets/img/brands/oppo.png" alt=""/>
+                        <img src = {Oppo} alt=""/>
                     </a>
                 </div>
 
                 <div className ="item">
                     <a href="#">
-                        <img src="./assets/img/brands/hurley.png" alt=""/>
+                        <img src = {Samsung} alt=""/>
                     </a>
                 </div>
 
