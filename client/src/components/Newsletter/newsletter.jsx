6f568ffb68b7b7b6a4270b021c5f4dd0b126f877
solomon-winner <solyalew3@gmail.com>
2024-01-16 19:21:17 +0300

    ...

--- client/src/components/Newsletter/newsletter.jsx
+++ client/src/components/Newsletter/newsletter.jsx
@@ -2,9 +2,23 @@ import "./newsletter.css"
 
 const Newsletter = () => {
     return (
-        <div className="newsletter">
-
-        </div>
+        <div class="newsletter">
+                <div class="container">
+                    <div class="wrapper">
+                        <div class="box">
+                            <div class="content">
+                                <h3>Subscribe to our Newsletter</h3>
+                                <p>Get E-mail updates about our latest shop and <strong>Special Offers!</strong></p>
+                            </div>
+                            <form action="" class="search">
+                                <span class="icon-large"><i class="ri-mai-line"></i></span>
+                                <input type="mail" placeholder="Your Email Addres" required/>
+                                <button type = "submit">Sign Up</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 }
 
