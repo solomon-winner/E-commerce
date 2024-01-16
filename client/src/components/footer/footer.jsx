50ceb9f27c5d6b31fc80699be671131a6abd4b6a
solomon-winner <solyalew3@gmail.com>
2024-01-16 19:31:01 +0300

    ...

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import Newsletter from "../Newsletter/newsletter";
+import Widgets from "../Widgets/widgets";
+import Info from "../Info/info";
+
+const Footer = () => {
+    return (
+        <>
+        <Newsletter/>
+        <Widgets/>
+        <Info/>
+        </>
+    )
+}
+export default Footer;
