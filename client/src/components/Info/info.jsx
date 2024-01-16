591ed2ea253df1c11ab023e985fbe973b50053c3
solomon-winner <solyalew3@gmail.com>
2024-01-16 19:17:14 +0300

    ...

---
+++ client/src/components/Info/info.jsx
@@ -0,0 +1,30 @@
+import "./info.css"
+
+const Info = () => {
+    return (
+        <div class="info">
+            <div class="container">
+                <div class="wrapper">
+                    <div class="flexcol">
+                        <div class="logo">
+                            <a href="#"><span class="circle"></span>.suk</a>
+                        </div>
+                        <div class="socials">
+                            <ul class="flexitem">
+                                <li><a href="#"><i class="ri-xxx-line"></i></a></li>
+                                <li><a href="#"><i class="ri-xxx-line"></i></a></li>
+                                <li><a href="#"><i class="ri-xxx-line"></i></a></li>
+                                <li><a href="#"><i class="ri-xxx-line"></i></a></li>
+                                <li><a href="#"><i class="ri-xxx-line"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p class="mini-text">copyright 2024 &copy; .suk. All right Reserved</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Info;
--- client/src/components/Widgets/widgets.jsx
+++ client/src/components/Widgets/widgets.jsx
@@ -2,7 +2,74 @@ import "./widgets.css";
 
 const Widgets = () => {
     return (
-        <div className="widgets"></div>
+        <div className ="widgets">
+                <div className ="containers">
+                    <div className ="wrapper">
+                        <div className ="flexwrap">
+                            <div className ="row">
+                                <div className ="item mini-links">
+                                    <h4>Help & Contact</h4>
+                                    <ul className ="flexcol">
+                                        <li><a href="">Your Account</a></li>
+                                        <li><a href="">Your Orders</a></li>
+                                        <li><a href="">Shipping Rates</a></li>
+                                        <li><a href="">Returns</a></li>
+                                        <li><a href="">Assistant</a></li>
+                                        <li><a href="">Help</a></li>
+                                        <li><a href="">Contact</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className ="row">
+                                <div className ="item mini-links">
+                                    <h4>Help & Contact</h4>
+                                    <ul className ="flexcol">
+                                        <li><a href="">Your Account</a></li>
+                                        <li><a href="">Your Orders</a></li>
+                                        <li><a href="">Shipping Rates</a></li>
+                                        <li><a href="">Returns</a></li>
+                                        <li><a href="">Assistant</a></li>
+                                        <li><a href="">Help</a></li>
+                                        <li><a href="">Contact</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className ="row">
+                                <div className ="item mini-links">
+                                    <h4>Help & Contact</h4>
+                                    <ul className ="flexcol">
+                                        <li><a href="">Your Account</a></li>
+                                        <li><a href="">Your Orders</a></li>
+                                        <li><a href="">Shipping Rates</a></li>
+                                        <li><a href="">Returns</a></li>
+                                        <li><a href="">Assistant</a></li>
+                                        <li><a href="">Help</a></li>
+                                        <li><a href="">Contact</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className ="row">
+                                <div className ="item mini-links">
+                                    <h4>Help & Contact</h4>
+                                    <ul className ="flexcol">
+                                        <li><a href="">Your Account</a></li>
+                                        <li><a href="">Your Orders</a></li>
+                                        <li><a href="">Shipping Rates</a></li>
+                                        <li><a href="">Returns</a></li>
+                                        <li><a href="">Assistant</a></li>
+                                        <li><a href="">Help</a></li>
+                                        <li><a href="">Contact</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 }
 export default Widgets;
