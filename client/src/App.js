95ecd5a09f79a806edec1997cf65cb06d3488f00
solomon-winner <solyalew3@gmail.com>
2024-01-17 10:27:50 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from "./pages/Home/home";
-
+import Profile from "./pages/profile/Profile"
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     {/* <Home/> */}
+     <Profile/>
     </div>
   );
 }
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,11 @@
 import "./Profile.css";
-
+import Trending from "../../components/Trending/trending"
+import Navbar from "../../components/navbar/Navbar";
 const Profile = () => {
     return (
         <div className="profile">
-
+            <Navbar/>
+            <Trending/>
         </div>
     )
 }
