682ed781c5fd9a14b81852127d89366ba7cd3c0d
solomon-winner <solyalew3@gmail.com>
2024-01-18 13:24:23 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,43 @@
 import './App.css';
 import AboutProduct from './pages/AboutPoduct/AboutProduct';
-// import Home from "./pages/Home/home";
-// import Profile from "./pages/profile/Profile"
+import Home from "./pages/Home/home";
+import Profile from "./pages/profile/Profile"
+import {createBrowserRouter,
+        RouterProvider,
+        Outlet,
+      Navigate,
+      BrowserRouter,
+      Routes,
+      Route} from "react-router-dom";
+// import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 
 function App() {
+  // const Layout = () => {
+  //   return (
+  //     <QueryClientProvider>
+  //       <>
+        
+  //       </>
+  //     </QueryClientProvider>
+  //   )
+  //}
+
+  
   return (
-    <div className="App">
-     {/* <Home/> */}
-     {/* <Profile/> */}
-     <AboutProduct/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/product' element = {<AboutProduct/>}/>
+    <Route path = '/profile' element = {<Profile/>}/>
+   </Routes>
+   </BrowserRouter>
+
   );
 }
 
 export default App;
+//  <div className="App">
+//      {/* <Home/> */}
+//      {/* <Profile/> */}
+//      <AboutProduct/>
+//     </div>
