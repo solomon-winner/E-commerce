7c8a8ef573b9611839dcc20ca2c92d6e31a49569
solomon-winner <solyalew3@gmail.com>
2024-01-18 17:00:51 +0300

    ...

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import { Cartslice } from "./Cartslice";
+import { Wishslice } from "./Wishslice";
+import { CommentSlice } from "./CommentSlice";
 
-export const CartActions = Cartslice.actions;
 const store = configureStore({
     reducer: {
-        Cart: Cartslice.reducer
+        Cart: Cartslice.reducer,
+        Wish: Wishslice.reducer,
+        Comment: CommentSlice.reducer,
     }
 })
+export default store;
