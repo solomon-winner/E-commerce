f572a698ab34f6e0f56b9ba1c97170ffe7cd0571
solomon-winner <solyalew3@gmail.com>
2024-01-19 15:54:04 +0300

    ...

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -4,12 +4,14 @@ import Cross from "../../assets/icons/xmark-solid.svg"
 const Cart = () => {
     return (
         <div className="cart">
-            <div className="container">
-                <div className="wrapper">
                     <div className="cartlist">
                         <div className="cartHead">
-                            <span>Cart List</span>
-                            <img src= {Cross} alt="" />
+                            <div className="cartlefthead">
+                               <div className="main-text">Cart list</div>
+                            <div className="mini-text mobile-hide">Total 1059 Products</div> 
+                            </div>
+                        
+                            <img className = "cross" src= {Cross} alt="" />
                         </div>
                         <div className="cartItem">
                             <span>Shoes</span>
@@ -18,13 +20,11 @@ const Cart = () => {
                             <span>Shoes</span>
                             <span>Shoes</span>
                             <span>Shoes</span>
-
+                            <span><button>Payment</button></span>
                         </div>
-                        <button>Payment</button>
+                        
                     </div>
                 </div>
-            </div>
-        </div>
     )
 }
 export default Cart;
