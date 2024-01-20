0391558d15a2e26a260d0041e16e5986b803e414
solomon-winner <solyalew3@gmail.com>
2024-01-20 17:46:24 +0300

    ..here started the back end...

--- api/index.js
+++ api/index.js
@@ -4,9 +4,15 @@ import ProductRoutes from "./routes/Product.js"
 import ProductsRoutes from "./routes/Products.js"
 import WishRoutes from "./routes/Wish.js"
 import HistoryRoutes from "./routes/History.js"
+import cors from "cors";
 
 const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 })
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
