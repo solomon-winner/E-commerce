9d647ffc29b71f53a69279c371cb8ae66bfa995d
solomon-winner <solyalew3@gmail.com>
2024-01-22 19:33:17 +0300

    ...Authentication page...

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -20,6 +20,22 @@ const Login = () => {
                     <button>Sign Up</button>
                 </form>
             </div>
+            <div className="toggle-container">
+                <div className="toggle">
+                    <div className="toggle-panel toggle-left">
+                        <h1>Welcome Back!</h1>
+                        <p>Fill the Following fields</p>
+                        <button className="hidden" id="login">Sign In</button>
+                    </div>
+                    <div className="toggle-panel toggle-right">
+                        <h1>Hello, Friend</h1>
+                        <p>Register Here!</p>
+                        <button className="hidden" id="register">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
