58e2ae12c517c9d35484c46fe3e8098b0f632777
solomon-winner <solyalew3@gmail.com>
2024-01-22 20:11:52 +0300

    ...Authentication page...

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -20,6 +20,23 @@ const Login = () => {
                     <button>Sign Up</button>
                 </form>
             </div>
+            <div className="form-container sign-in">
+                <form >
+               <h1>Sign In</h1>
+                <div className = "social-icons">
+                    <a href="#" className = "icon"><i className = "fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" className = "icon"><i className = "fa-brands fa-facebook-f"></i></a>
+                    <a href="#" className = "icon"><i className = "fa-brands fa-github"></i></a>
+                    <a href="#" className = "icon"><i className = "fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email password</span>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <a href="#">Forget Your Password?</a>
+                <button>Sign In</button>
+                </form>
+            </div>
+            
             <div className="toggle-container">
                 <div className="toggle">
                     <div className="toggle-panel toggle-left">
