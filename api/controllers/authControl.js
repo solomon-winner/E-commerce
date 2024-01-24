475fddf15e1e0dca0b13621946f954d93aa54d9b
solomon-winner <solyalew3@gmail.com>
2024-01-24 22:08:10 +0300

    ...

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -9,10 +9,18 @@ export const register = (req, res) => {
     const query = "SELECT * FROM User WHERE Email = ?"
     const email = req.body.Email;
 
-    db.query(query, [req.body.Email], (err, data) => {
+    db.query(query, [email], (err, data) => {
         if (err) return res.status(500).json(err);
         if (data.length) return res.status(409).json(" User already exists!");
         const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(req.body.Password, salt);
+
+        const query = "INSERT INTO User (`Name`,`Nitche`,`Bank`,`Bankaccount`,`Password`,`Email`,`Country`,`City`,`subCity`,`street`) VALUES (?)"
+        const values = [req.body.Name, req.body.Nitche, req.body.Bank,
+             req.body.Bankaccount, req.body.Password, req.body.Email, 
+             req.body.Country, req.body.City, req.body.subCity, 
+             req.body.street]
+             
     })
 }
 export const logout = () =>{}
