086e3ee6abff6dbd28b3e14b425b0dd96b96509e
solomon-winner <solyalew3@gmail.com>
2024-01-24 22:30:40 +0300

    ...

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -21,6 +21,10 @@ export const register = (req, res) => {
              req.body.Country, req.body.City, req.body.subCity, 
              req.body.street]
              
+             db.query(query, [values], (err, data) => {
+                if (err) return res.status(500).json(err);
+                return res.status(200).json("User has been Created!")
+             })
     })
 }
 export const logout = () =>{}
