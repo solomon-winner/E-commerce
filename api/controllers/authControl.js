6cb6f38748ec5fda1af2f0f3c9500cd78fe07b5d
solomon-winner <solyalew3@gmail.com>
2024-01-24 21:53:37 +0300

    ...

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -8,6 +8,11 @@ export const login = (req, res) => {
 export const register = (req, res) => {
     const query = "SELECT * FROM User WHERE Email = ?"
     const email = req.body.Email;
-    console.log(req)
+
+    db.query(query, [req.body.Email], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.length) return res.status(409).json(" User already exists!");
+        const salt = bcrypt.genSaltSync(10);
+    })
 }
 export const logout = () =>{}
