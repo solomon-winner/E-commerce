a89289b965984dcd546eca113b851cfeb782a362
solomon-winner <solyalew3@gmail.com>
2024-02-02 15:15:39 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import Home from "./pages/Home/home";
 import Profile from "./pages/profile/Profile"
 import {
       Navigate,
-      createBrowserRouter,
-      RouterProvider,
+      BrowserRouter as Router,
+      Routes,
+      Route,
       Outlet} from "react-router-dom";
 import Authentication from "./pages/Authentication/Authentication"
 import { useContext } from 'react';
@@ -16,14 +17,14 @@ function App() {
  
   const queryClient = new QueryClient();
 
-const Layout = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-<Outlet/>
-</QueryClientProvider>
-  )
+// const Layout = () => {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+// <Outlet/>
+// </QueryClientProvider>
+//   )
 
-}
+//}
 const ProtectedRoute = ({children}) => {
   if (!current) {
     return <Navigate to = "/Authentication" />
@@ -31,34 +32,34 @@ const ProtectedRoute = ({children}) => {
   return children;
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <Layout/>
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/profile/:id",
-        element: <Profile/>,
-      },
-    ]
-  },
-  {
-    path: "/Authentication",
-    element: <Authentication/>
-  },
-  {
-    path: "/product",
-    element: <AboutProduct/>,
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <ProtectedRoute>
+//         <Layout/>
+//       </ProtectedRoute>
+//     ),
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/profile/:id",
+//         element: <Profile/>,
+//       },
+//     ]
+//   },
+//   {
+//     path: "/Authentication",
+//     element: <Authentication/>
+//   },
+//   {
+//     path: "/product",
+//     element: <AboutProduct/>,
+//   }
+// ])
   
   return (
   //  <BrowserRouter>
@@ -69,7 +70,14 @@ const router = createBrowserRouter([
   //  </Routes>
   //  </BrowserRouter>
   // <RouterProvider router = {router} />
-  <RouterProvider router={router}/>
+  // <RouterProvider router={router}/>
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+</QueryClientProvider>
   );
 }
 
